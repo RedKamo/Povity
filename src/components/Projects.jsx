f4c01d2e274c1801e57af5e15f1cc4e5bf0e0360
RedKamo <camiloandressuarez1@gmail.com>
2021-01-15 18:40:37 -0500

    finish main page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 //import react icons
-import { FaPlus } from 'react-icons/fa';
-import { RiArrowDropUpLine , RiArrowDropDownLine  } from "react-icons/ri";
-
+import { FaPlus, FaTrash } from 'react-icons/fa';
+//import { RiArrowDropUpLine , RiArrowDropDownLine  } from "react-icons/ri";
+import { AiOutlineMore } from 'react-icons/ai';
 
 //import styles
 import '../assets/styles/Projects.scss';
@@ -16,16 +16,15 @@ const Projects =()=>{
         <input type="text" placeholder="Add your task here... " />
         <button><FaPlus /> Add</button>
       </div>
-      <h1  className="project__title">TODAY TODO LIST</h1>
+      <h3  className="project__title">TODAY TODO LIST</h3>
       <div className="projects__activities">
-        <p className="projects__activities--title">Create login and register wireframes</p>
-        <div className="projects__activities--drop">
-          <span>04</span>
-          <div className="projects__activities--arrows">
-          <RiArrowDropUpLine/><RiArrowDropDownLine/> 
-          </div>
-          
+        <p className="projects__activities--title">Create login and register wireframes</p>        
+        <span className="projects__activities--drop">04</span> 
+        <div className="projects__activities--icons">
+          <FaTrash/>
+          <AiOutlineMore/>
         </div>
+          
       </div>     
     </div>
   )
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
+import { FaPlay, FaPause } from 'react-icons/fa';
+
+
 import '../assets/styles/Timer.scss';
 
 
 const Timer =()=>{
-    return(
-        <div className="timer__container">
-            <h1>Timer column</h1>
+  return(
+    <div className="timer">
+      <div className="timer__pomodoro">
+        <h1 className="timer__pomodoro__clock">25:00</h1>
+        <div className="timer__pomodoro__icons">
+          <FaPlay size={40} />
+          <FaPause size={40} />
         </div>
+        <div className="timer__pomodoro__buttons">
+          <button>Pomodoro</button>
+          <button>Short break</button>
+          <button>Long break</button>
+        </div>
+      </div>
+      <div className="timer__statics">
+        <h2>TODAY STATICS</h2>
+      </div>
+
+    </div>
     )
 }
 

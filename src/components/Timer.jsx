dba51d279f410f5794a6959d87130795c95933c7
RedKamo <camiloandressuarez1@gmail.com>
2021-02-17 21:44:19 -0500

    add some changes on timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
-import { FaPlay, FaPause } from 'react-icons/fa';
+
+
+//import { FaPlay, FaPause } from 'react-icons/fa';
 
 
 import '../assets/styles/Timer.scss';
 
 
-const Timer =()=>{
+const Timer =()=> {
+
+  const [ timer, setTimer ] = useState(0)
+  const [ isActive, setisActive ] = useState(false)
+  const countRef = useRef(null)
+
+  const handlePlay = () => {
+    if (isActive === true) {
+      clearInterval(countRef.current)
+      setisActive(false);
+    }
+    else if (timer > 1 ){
+      setisActive(true);
+        countRef.current = setInterval(()=>{
+          setTimer((timer)=> timer -1 )
+        }, 1000)
+    }
+  }
+
+  const handleStop = ()=> {
+    clearInterval(countRef.current)
+    setisActive(false)
+    setTimer(25)
+  }
+
+  const handleDecrease = ()=>{
+    setTimer(timer -1 )
+  }
+
+  const handleIncrease = ()=>{
+    setTimer(timer + 1)
+  }
+
+
   return(
     <div className="timer">
       <div className="timer__pomodoro">
-        <h1 className="timer__pomodoro__clock">25:00</h1>
+        <button onClick={handleDecrease}> - </button>
+          {}
+          <p>{timer}</p>
+          {/* <h1 className="timer__pomodoro__clock">25:00</h1> */}
+          <button onClick={handleIncrease}> + </button>
         <div className="timer__pomodoro__icons">
-          <FaPlay size={40} />
-          <FaPause size={40} />
+         
         </div>
         <div className="timer__pomodoro__buttons">
-          <button>Pomodoro</button>
+          <button onClick={handlePlay}>start</button>
+          <button onClick={handleStop}>pause</button>
+         {/*  <button>Pomodoro</button>
           <button>Short break</button>
-          <button>Long break</button>
+          <button>Long break</button> */}
         </div>
       </div>
       <div className="timer__statics">
